67094f02d453fff21da61103d9dd2e78ae659952
Eghosa Ordia <eghordia130@gmail.com>
2022-10-27 22:18:46 +0100

    changes

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 "use strict";
+const body = document.querySelector('body')
 const question_div = document.querySelectorAll(".acc-div");
 const answer = document.querySelectorAll(".acc-ans");
-const toggle_div = document.querySelector('.toggle')
-const toggle_btn = document.querySelector('.t-btn')
-const btn_change = document.querySelector('.fa-sun')
+const toggle_div = document.querySelector(".toggle");
+const toggle_btn = document.querySelector(".t-btn");
+const btn_change = document.querySelector(".fa-sun");
 
 question_div.forEach((div) => {
   div.addEventListener("click", () => {
@@ -20,6 +21,14 @@ question_div.forEach((div) => {
   });
 });
 
-toggle_btn.addEventListener('click', () => {
-  toggle_btn.classList.toggle('move')
-})
+toggle_btn.addEventListener("click", () => {
+  if (btn_change.classList.contains("fa-sun")) {
+    btn_change.classList.remove("fa-sun");
+    btn_change.classList.add("fa-moon");
+  } else {
+    btn_change.classList.remove("fa-moon");
+    btn_change.classList.add("fa-sun");
+  }
+  toggle_btn.classList.toggle("move");
+  body.classList.toggle('change')
+});
